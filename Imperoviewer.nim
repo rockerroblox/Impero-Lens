de2b6f1de37d6dda5dc81110355efca7d0b9d3cc
rockerroblox <68230393+rockerroblox@users.noreply.github.com>
2026-01-21 21:06:41 +0000

    Update Imperoviewer.nim

--- Imperoviewer.nim
+++ Imperoviewer.nim
@@ -1,81 +1,38 @@
 import os
 
+let run = getAppDir()
+let player = run / "ScreenRecordingPlayer.exe"
 
-var run = getAppDir()
-
-# Pre-flight checks
-
-var player = run & "\\ScreenRecordingPlayer.exe"
-if fileExists(player):
-    # Works, continue
-    discard
-else:
+if not fileExists(player):
     echo "ScreenRecordingPlayer.exe not found."
     quit(1)
-
-
 
 echo "Welcome to Impero Viewer. This lets you view your screen recordings."
 echo "Please locate your Impero installer directory if an error is shown."
 
-var imppath = "C:\\Program Files (x86)\\Impero Solutions Ltd\\Impero Client\\Recordings\\PUBLIC"
-var impnoerr = "C:\\Program Files (x86)\\Impero Solutions Ltd\\Impero Client"
+let imppath = r"C:\Program Files (x86)\Impero Solutions Ltd\Impero Client\Recordings\PUBLIC"
+let impnoerr = r"C:\Program Files (x86)\Impero Solutions Ltd\Impero Client"
 
-proc checkImp1(): bool =
-    if dirExists(imppath):
-        return true
-    elif not dirExists(imppath):
-        return false
+proc findAndPlay(directory: string) =
+    for file in walkFiles(directory / "*.irf"):
+        let (_, name, _) = splitFile(file)
+        echo "Found recording " & name
+        echo "Viewing recording..."
+        let cmd = player.quoteShell() & " filepath=" & file.quoteShell()
+        discard execShellCmd(cmd)
+        echo "Here is your recording."
 
-proc checkImpMain(): bool =
-    if dirExists(impnoerr):
-        return true
-    elif not dirExists(impnoerr):
-        return false
-
-var Imprec = checkImp1()
-var Impain = checkImpMain()
-
-if not Imprec and not Impain:
-    echo "Checking current directory..."
-    for file in walkFiles(getCurrentDir()):
-        let (_, name, ext) = splitFile(file)
-        if ext == ".irf":
-            echo "Found recording " & name
-            echo "Viewing recording..."
-            var cmd = player.quoteShell() & " filepath=" & file.quoteShell()
-            discard os.execShellCmd(cmd)
-            echo "Here is your recording."
-
-
-if Impain and not Imprec:
-    if fileExists(impnoerr & "\\Recordings"):
+if dirExists(imppath):
+    echo "Found recording dir"
+    findAndPlay(imppath)
+elif dirExists(impnoerr):
+    let subDir = impnoerr / "Recordings" / "PUBLIC"
+    if dirExists(subDir):
         echo "Found Recording directory."
-        echo "Checking for recordings"
-        for file in walkFiles(impnoerr & "\\Recordings\\PUBLIC"):
-            echo "Found file " & file
-            let (_, name, ext) = splitFile(file)
-            if ext == ".irf":
-                echo "Found recording " & name
-                echo "Viewing recording..."
-                var cmd = player.quoteShell() & " filepath=" & file.quoteShell()
-                discard os.execShellCmd(cmd)
-                echo "Here is your recording."
-    elif not fileExists(impnoerr & "\\Recordings"):
+        findAndPlay(subDir)
+    else:
         echo "No recordings found."
         quit(1)
-
-if Imprec and Impain:
-    echo "Found recording dir"
-    echo "Checking dir."
-    for file in walkFiles(imppath):
-        let (_, name, ext) = splitFile(file)
-        if ext == ".irf":
-            echo "Found recording " & name
-            echo "Viewing recording..."
-            var cmd = player.quoteShell() & " filepath=" & file.quoteShell()
-            discard os.execShellCmd(cmd)
-            echo "Here is your recording."
-
-
-
+else:
+    echo "Checking current directory..."
+    findAndPlay(getCurrentDir())
